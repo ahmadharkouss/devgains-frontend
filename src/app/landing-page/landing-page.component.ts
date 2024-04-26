d8e2c2fcda6a7beaef4d22c7a23fae55555d2f3c
ahmadharkouss <71528971+ahmadharkouss@users.noreply.github.com>
2024-04-26 15:21:34 +0200

    added all animations

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,7 +7,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import {tuiFadeIn} from '@taiga-ui/core';
 import anime from 'animejs';
-
+import AOS from "aos";
+import scrollmagic from 'scrollmagic';
 
 @Component({
   selector: 'app-landing-page',
@@ -32,7 +33,15 @@ export class LandingPageComponent {
   }
      ).subscribe();
 }
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  AOS.init();
+}
+
 ngAfterViewInit(): void{
+  
 
 
 
